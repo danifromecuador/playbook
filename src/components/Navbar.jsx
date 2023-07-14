3848882ca5f90a7f16672fca5462736fcb33afa3
dani <danifromecuador@gmail.com>
2023-07-14 13:55:47 -0500

    Change Navbar link name to Books as the Home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <h1>Bookstore CMS</h1>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">Books</Link>
         </li>
         <li>
           <Link to="/categories">Categories</Link>
