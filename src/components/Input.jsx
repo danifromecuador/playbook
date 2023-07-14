34c83d57106bf459ee7d6ac1c0b7a53f3d7dcfda
dani <danifromecuador@gmail.com>
2023-07-14 13:52:45 -0500

    Create inputs for add a new book in Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,13 @@
 const Input = () => {
   return (
-    <div>Input</div>
+    <div className="input">
+      <h3>ADD NEW BOOK</h3>
+      <form>
+        <input type="text" placeholder="Book title" />
+        <input type="text" placeholder="Author" />
+        <button>ADD BOOK</button>
+      </form>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav>
+      <h1>Bookstore CMS</h1>
       <ul>
         <li>
           <Link to="/">Home</Link>
