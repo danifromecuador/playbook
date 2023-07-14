4b1623a6c223d0e64d89037dace85217c6e18a92
dani <danifromecuador@gmail.com>
2023-07-14 13:35:44 -0500

    Create a Navbar component to navigate between 2 pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Categories from './pages/Categories';
+import './App.css';
 
 const App = () => {
-
   return (
     <div className="App">
-      hello world
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/categories">Categories</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
