31497751d81891c88ab45c872a706ce651d35752
dani <danifromecuador@gmail.com>
2023-07-19 14:22:29 -0500

    Add a reducer to add a book

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,12 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  array: [], 
+  array: [],
 }
 
 const booksSlice = createSlice({
+
+
   name: "books",
   initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.array.push(action.payload)
+    }
+  }
 })
 
+export const { addBook } = booksSlice.actions
 export default booksSlice.reducer
