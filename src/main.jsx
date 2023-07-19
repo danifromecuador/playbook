43edbd92edd1395eb7094a76d3db572fccde2252
dani <danifromecuador@gmail.com>
2023-07-19 13:04:27 -0500

    Configure the store with an empty reducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
+
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
+
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
 )
