cb81fd5274a1d39da8dfd8a1dbc61396bb4cfa6f
dani <danifromecuador@gmail.com>
2023-07-21 12:10:45 -0500

    Delete a book based in its index

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,9 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
-import array from './array'
+import array from './array';
 
 const initialState = {
   array: array,
-}
+};
 
 const booksSlice = createSlice({
   name: "books",
@@ -14,19 +14,24 @@ const booksSlice = createSlice({
         item_id: `item${state.array.length + 1}`,
         title: action.payload.title,
         author: action.payload.author,
-        category: "unknown"
+        category: "unknown",
       };
       return {
         ...state,
         array: [...state.array, newBook],
-      }
+      };
     },
-    removeBook: (state = initialState, action) => {
-      console.log(action.payload.index)
-      return state;
-    }
-  }
-})
 
-export const { addBook, removeBook } = booksSlice.actions
-export default booksSlice.reducer
+    removeBook: (state = initialState, action) => {
+      const newArray = [...state.array];
+      newArray.splice(action.payload.index, 1);
+      return {
+        ...state,
+        array: newArray,
+      };
+    },
+  },
+});
+
+export const { addBook, removeBook } = booksSlice.actions;
+export default booksSlice.reducer;
