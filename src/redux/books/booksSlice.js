f8866d4f91a86e96031cab942b5f8b9b8f10c72f
dani <danifromecuador@gmail.com>
2023-07-20 19:13:32 -0500

    Restore the array to its original initialState

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -2,20 +2,26 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   array:
-    [
-      {
-        "item_id": "item1",
-        "title": "The Great Gatsby",
-        "author": "John Smith",
-        "category": "Fiction"
-      },
-      {
-        "item_id": "item2",
-        "title": "Anna Karenina",
-        "author": "Leo Tolstoy",
-        "category": "Fiction"
-      }
-    ],
+  [
+    {
+      "item_id": "item1",
+      "title": "The Great Gatsby",
+      "author": "John Smith",
+      "category": "Fiction"
+    },
+    {
+      "item_id": "item2",
+      "title": "Anna Karenina",
+      "author": "Leo Tolstoy",
+      "category": "Fiction"
+    },
+    {
+      "item_id": "item3",
+      "title": "The Selfish Gene",
+      "author": "Richard Dawkins",
+      "category": "Nonfiction"
+    }
+  ],
 }
 
 const booksSlice = createSlice({
