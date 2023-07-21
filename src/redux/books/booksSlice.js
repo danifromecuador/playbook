c92d624a7fe157ee678c875732e5feafc2a140fa
dani <danifromecuador@gmail.com>
2023-07-20 19:37:12 -0500

    Move the array initialState from the booksSlice to a new file

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,27 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import array from './array'
 
 const initialState = {
-  array:
-  [
-    {
-      "item_id": "item1",
-      "title": "The Great Gatsby",
-      "author": "John Smith",
-      "category": "Fiction"
-    },
-    {
-      "item_id": "item2",
-      "title": "Anna Karenina",
-      "author": "Leo Tolstoy",
-      "category": "Fiction"
-    },
-    {
-      "item_id": "item3",
-      "title": "The Selfish Gene",
-      "author": "Richard Dawkins",
-      "category": "Nonfiction"
-    }
-  ],
+  array: array,
 }
 
 const booksSlice = createSlice({
