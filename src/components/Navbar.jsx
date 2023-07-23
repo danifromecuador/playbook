bab53bec787968e0019f601601058e946f56c041
dani <danifromecuador@gmail.com>
2023-07-22 23:53:37 -0500

    Adjust margin and padding of Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,20 @@ import '../styles/Navbar.css'
 
 const Navbar = () => {
   return (
-    <nav>
-      <h1>Bookstore CMS</h1>
-      <ul>
-        <li>
-          <Link to="/">BOOKS</Link>
-        </li>
-        <li>
-          <Link to="/categories">CATEGORIES</Link>
-        </li>
-      </ul>
-      <div className="img">👤</div>
-    </nav>
+    <div className="navContainer">
+      <nav>
+        <h1>Bookstore CMS</h1>
+        <ul>
+          <li>
+            <Link to="/">BOOKS</Link>
+          </li>
+          <li>
+            <Link to="/categories">CATEGORIES</Link>
+          </li>
+        </ul>
+        <div className="img">👤</div>
+      </nav>
+    </div>
   );
 };
 
